0cf7fcc878b3a62abfb1392c50496e53e865841c
din <dina.vintr@gmail.com>
2024-11-01 19:04:18 +0200

    delete playground

--- libs/playground/src/store.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from "@atomico/store";
-
-export const EditorStore = createStore({
-    code: `import { c } from 'atomico';
-           import '@gigya/screen-set`,
-    lang: "tsx"
-});
-
